b3788bb0afad3257d2b8390a8d5f1a3cde251655
pythonstrup <pythonstrup@gmail.com>
2025-01-20 22:19:16 +0900

    위젯이란?

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -6,10 +6,11 @@ void main() {
   runApp(
     // MaterialApp은 항상 최상위에 위치한다.
     // Scaffold는 바로 아래에 위치한다. (기본 구조를 쉽게 만들 수 있게 해주는 것이 scaffold)
-    MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text(
+    // Widget: 화면에 무언가를 보여주는 모든 요소
+    const MaterialApp(   // Material 디자인을 사용할 수 있게 해주는 위젯
+      home: Scaffold(    // 화면에 구조를 만들 수 있게 해주는 위젯
+        body: Center(    // 가운데 정렬을 해주는 위젯
+          child: Text(   // 글자를 화면에 그려주는 위젯
             'Hello World'
           )
         ),
