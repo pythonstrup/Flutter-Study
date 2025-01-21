8ad78138a3839b32ff48a9da73f14dec046aeb32
pythonstrup <pythonstrup@gmail.com>
2025-01-21 21:56:44 +0900

    Stateless Widget

--- splash_screen/lib/main.dart
+++ splash_screen/lib/main.dart
@@ -3,19 +3,28 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        // 원하는 색상 코드 #335CB0
-        backgroundColor: Color(0xFF335CB0),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('asset/img/logo.png'),
-            CircularProgressIndicator(
-              color: Colors.white,
-            ),
-          ],
-        ),
-      ),
+      home: HomeScreen()
     ),
   );
+}
+
+// Stateless Widget
+class HomeScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // 원하는 색상 코드 #335CB0
+      backgroundColor: Color(0xFF335CB0),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('asset/img/logo.png'),
+          CircularProgressIndicator(
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
 }
