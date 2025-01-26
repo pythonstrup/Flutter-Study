a88956050722daf086c1b65d25c81e420d2111cb
pythonstrup <pythonstrup@gmail.com>
2025-01-26 22:56:37 +0900

    safe area

--- row_and_column/lib/screen/home_screen.dart
+++ row_and_column/lib/screen/home_screen.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:row_and_column/const/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            children: colors
+                .map(
+                  (color) => Container(
+                    height: 50.0,
+                    width: 50.0,
+                    color: color,
+                  ),
+                )
+                .toList(),
+          ),
+        ),
+      ),
+    );
   }
 }
