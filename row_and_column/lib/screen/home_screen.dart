4cd9439dbac529a2942d5e730d8e1a21229b7faf
pythonstrup <pythonstrup@gmail.com>
2025-01-26 23:08:26 +0900

    MainAxisAlignment & CrossAxisAlignment

--- row_and_column/lib/screen/home_screen.dart
+++ row_and_column/lib/screen/home_screen.dart
@@ -8,20 +8,48 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          color: Colors.black,
-          child: Column(
-            children: colors
-                .map(
-                  (color) => Container(
-                    height: 50.0,
-                    width: 50.0,
-                    color: color,
-                  ),
-                )
-                .toList(),
+        child: Stack(children: [
+          Container(
+            color: Colors.black,
+            // 넓이를 끝까지 늘려줌
+            width: double.infinity,
+            child: Column(
+              // mainAxisAlignment의 기본값은 start
+              mainAxisAlignment: MainAxisAlignment.end,
+              // mainAxisSize는 기본값이 max다.
+              mainAxisSize: MainAxisSize.max,
+              // crossAxisAlignment의 기본값은 center
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: colors
+                  .map(
+                    (color) => Container(
+                      height: 50.0,
+                      width: 50.0,
+                      color: color,
+                    ),
+                  )
+                  .toList(),
+            ),
           ),
-        ),
+          // Container(
+          //   color: Colors.black,
+          //   // 높이를 끝까지 늘려줌
+          //   height: double.infinity,
+          //   child: Row(
+          //     mainAxisAlignment: MainAxisAlignment.center,
+          //     crossAxisAlignment: CrossAxisAlignment.center,
+          //     children: colors
+          //         .map(
+          //           (color) => Container(
+          //             height: 50.0,
+          //             width: 50.0,
+          //             color: color,
+          //           ),
+          //         )
+          //         .toList(),
+          //   ),
+          // ),
+        ]),
       ),
     );
   }
