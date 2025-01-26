772d5e79e8cda3643ea185f12db9e30624732ece
pythonstrup <pythonstrup@gmail.com>
2025-01-26 22:46:42 +0900

    모듈 나누기

--- row_and_column/lib/main.dart
+++ row_and_column/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:row_and_column/screen/home_screen.dart';
 
 void main() {
   runApp(
-
+    const MaterialApp(
+      home: const HomeScreen(),
+    ),
   );
 }
-
-
