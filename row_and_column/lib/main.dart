4be20e7aa83da46e111e276bb986f0552b2b0e0b
pythonstrup <pythonstrup@gmail.com>
2025-01-26 23:13:06 +0900

    Expanded Widget

--- row_and_column/lib/main.dart
+++ row_and_column/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:row_and_column/screen/home_screen.dart';
+import 'package:row_and_column/screen/home_screen_expanded.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: const HomeScreen(),
+      home: const HomeScreenExpanded(),
     ),
   );
 }
---
+++ row_and_column/lib/screen/home_screen_expanded.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:row_and_column/const/colors.dart';
+
+class HomeScreenExpanded extends StatelessWidget {
+  const HomeScreenExpanded({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: Colors.black,
+          height: double.infinity,
+          child: Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  height: 50.0,
+                  width: 50.0,
+                  color: Colors.red,
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  height: 50.0,
+                  width: 50.0,
+                  color: Colors.orange,
+                ),
+              ),
+              Container(
+                height: 50.0,
+                width: 50.0,
+                color: Colors.yellow,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
