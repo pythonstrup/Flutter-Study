d60208749c33dca1f54b91d4bdd73b9770a5ee01
pythonstrup <pythonstrup@gmail.com>
2025-02-23 22:51:19 +0900

    image carousel: Timer Carousel

--- image_carousel/lib/screen/home_screen.dart
+++ image_carousel/lib/screen/home_screen.dart
@@ -1,13 +1,57 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Timer? timer;
+  PageController controller = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    timer = Timer.periodic(
+      Duration(seconds: 2),
+      (timer) {
+        int currentPage = controller.page!.toInt();
+        int nextPage = currentPage + 1;
+
+        if (nextPage > 4) {
+          nextPage = 0;
+        }
+
+        controller.animateToPage(
+          nextPage,
+          duration: Duration(microseconds: 500),
+          curve: Curves.linear,
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    if (timer != null) {
+      timer!.cancel();
+    }
+
+    controller.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: PageView(
+          controller: controller,
           children: [1, 2, 3, 4, 5]
               .map(
                 (number) => Image.asset(
